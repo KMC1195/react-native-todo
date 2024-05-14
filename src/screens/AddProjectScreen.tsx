d19cda1dd9b75aa0312bbb2e69abe661c1864998
kmc1195 <kmckrzysiekw@gmail.com>
2024-05-14 15:37:07 +0200

    Using native datetime picker instead of handmade one

--- src/screens/AddProjectScreen.tsx
+++ src/screens/AddProjectScreen.tsx
@@ -5,16 +5,18 @@ import {
   View,
   Pressable,
   Dimensions,
+  Platform,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {NavigationProp} from '@react-navigation/native';
 import {colors} from '../theme/colors';
 import StyledText from '../components/StyledText';
 import {ArrowLeftIcon} from 'react-native-heroicons/outline';
 import MyTextInput from '../components/MyTextInput';
 import MyButton from '../components/MyButton';
-import TimePicker from '../components/TimePicker';
-import DatePicker from '../components/DatePicker';
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from '@react-native-community/datetimepicker';
 
 interface Props {
   navigation: NavigationProp<any, any>;
@@ -22,58 +24,98 @@ interface Props {
 
 export default function AddProjectScreen({navigation}: Props) {
   const isDarkMode = useColorScheme() === 'dark';
+
+  // Datetimepicker logic
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState<'date' | 'time'>('date');
+  const [show, setShow] = useState(Platform.OS === 'ios' ? true : false);
+
+  function onChange(event: DateTimePickerEvent, date?: Date | undefined) {
+    const currentDate = date;
+    if (Platform.OS === 'android') {
+      setShow(false);
+    }
+    setDate(currentDate ? currentDate : new Date());
+  }
+
+  function showMode(currentMode: 'date' | 'time') {
+    setShow(true);
+    setMode(currentMode);
+  }
+  function showDatePicker() {
+    showMode('date');
+  }
+  function showTimePicker() {
+    showMode('time');
+  }
+
   return (
-    <>
-      <SafeAreaView
-        style={{
-          backgroundColor: isDarkMode ? colors.darkGray : 'white',
-          flex: 1,
-        }}>
-        <ScrollView
-          style={{paddingHorizontal: 10, flex: 1}}
-          showsVerticalScrollIndicator={false}>
-          <View style={{flexDirection: 'row', alignItems: 'center', gap: 15}}>
-            <Pressable onPress={() => navigation.goBack()}>
-              <ArrowLeftIcon
-                color={isDarkMode ? 'white' : colors.darkGray}
-                strokeWidth={2.5}
-                size={25}
-              />
-            </Pressable>
-            <StyledText styles={{fontSize: 30}}>Add Project</StyledText>
+    <SafeAreaView
+      style={{
+        backgroundColor: isDarkMode ? colors.darkGray : 'white',
+        flex: 1,
+      }}>
+      <ScrollView
+        style={{paddingHorizontal: 10, flex: 1}}
+        showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: 15,
+            padding: 5,
+          }}>
+          <Pressable onPress={() => navigation.goBack()}>
+            <ArrowLeftIcon
+              color={isDarkMode ? 'white' : colors.darkGray}
+              strokeWidth={2.5}
+              size={25}
+            />
+          </Pressable>
+          <StyledText styles={{fontSize: 30}}>Add Project</StyledText>
+        </View>
+
+        <View
+          style={{
+            marginTop: 20,
+            marginBottom: 10,
+          }}>
+          <View>
+            <StyledText>Title:</StyledText>
+            <MyTextInput placeholder="Enter a title..." />
+          </View>
+          <View style={{marginTop: 20}}>
+            <StyledText>Description:</StyledText>
+            <MyTextInput placeholder="Enter a title..." multiline />
           </View>
 
           <View
             style={{
-              marginTop: 20,
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+              marginTop: 30,
             }}>
-            <View>
-              <StyledText>Title:</StyledText>
-              <MyTextInput placeholder="Enter a title..." />
-            </View>
-            <View style={{marginTop: 20}}>
-              <StyledText>Description:</StyledText>
-              <MyTextInput placeholder="Enter a title..." multiline />
-            </View>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'space-evenly',
-                marginTop: 30,
-              }}>
-              <MyButton onPress={() => {}}>Choose date</MyButton>
-              <MyButton onPress={() => {}}>Choose time</MyButton>
-            </View>
-
-            <View style={{marginTop: 60}}>
-              <MyButton onPress={() => {}}>Add</MyButton>
-            </View>
+            <MyButton onPress={showDatePicker}>Choose date</MyButton>
+            <MyButton onPress={showTimePicker}>Choose time</MyButton>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-      {/* <TimePicker /> */}
-      <DatePicker />
-    </>
+
+          {show && (
+            <DateTimePicker
+              value={date}
+              mode={mode}
+              display="spinner"
+              is24Hour={true}
+              onChange={onChange}
+            />
+          )}
+
+          <StyledText>{`${date}`}</StyledText>
+
+          <View style={{marginTop: 60}}>
+            <MyButton onPress={() => {}}>Add</MyButton>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
